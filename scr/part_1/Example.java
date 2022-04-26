afede8d3a9818455524bb058dc666fac215c252b
Denis Lazykin <dlazykin856@gmail.com>
2022-04-26 10:02:39 +0300

    Простая программа на Java

---
+++ scr/part_1/Example.java
@@ -0,0 +1,10 @@
+package part_1;
+/*
+Простая программа на Java
+Присвоить исзодному файлу имя Example.java
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java");
+    }
+}
