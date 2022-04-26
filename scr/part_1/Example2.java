6c865f165b92517e35d37a5daa5bab9308c4eebc
Denis Lazykin <dlazykin856@gmail.com>
2022-04-26 10:10:45 +0300

    Еще одна простая программа

---
+++ scr/part_1/Example2.java
@@ -0,0 +1,15 @@
+package part_1;
+
+/*
+Еще одна простая программа
+ */
+public class Example2 {
+    public static void main(String[] args) {
+        int num; //обявили переменную с типом int под именем num
+        num = 100; //присваиваем num значение 100
+        System.out.println("Это переменная num: " + num);
+        num *= 2;
+        System.out.print("Значение переменной num * 2 равно ");
+        System.out.println(num);
+    }
+}
