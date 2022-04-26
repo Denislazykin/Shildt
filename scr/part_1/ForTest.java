91266c47308d1b53f9ad5ca450ead37ba657d61d
Denis Lazykin <dlazykin856@gmail.com>
2022-04-26 10:25:35 +0300

    Демонстрация применения цикла for

---
+++ scr/part_1/ForTest.java
@@ -0,0 +1,12 @@
+package part_1;
+/*
+Демонстрация применения цикла for
+ */
+public class ForTest {
+    public static void main(String[] args) {
+        int x;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Значение i: " + i);
+        }
+    }
+}
