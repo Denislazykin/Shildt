c75a1b69051d1421965a0fdfc849b14159b2daa5
Denis Lazykin <dlazykin856@gmail.com>
2022-04-27 09:36:23 +0300

    Демонстрация применения типа данных char

---
+++ scr/part_3/CharDemo.java
@@ -0,0 +1,14 @@
+package part_3;
+
+/*
+Демонстрация применения типа данных char
+ */
+public class CharDemo {
+    public static void main(String[] args) {
+        char c1, c2;
+        c1 = 88;
+        c2 = 'Y';
+        System.out.print("c1 и c2: ");
+        System.out.println(c1 + " " + c2);
+    }
+}
---
+++ scr/part_3/CharDemo2.java
@@ -0,0 +1,11 @@
+package part_3;
+
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+        ch1 = 'X';
+        System.out.println("c1 содержит " + ch1);
+        ch1++;
+        System.out.println("с1 теперь содежрит " + ch1);
+    }
+}
