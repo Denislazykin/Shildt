2da16006e1a80811c5835c4085b6f11c5366d762
Denis Lazykin <dlazykin856@gmail.com>
2022-04-27 09:41:27 +0300

    Демонстрация применения типа данных boolean

---
+++ scr/part_3/BoolTest.java
@@ -0,0 +1,19 @@
+package part_3;
+
+/*
+Демонстрация применения типа данных boolean
+ */
+public class BoolTest {
+    public static void main(String[] args) {
+        boolean b;
+        b = false;
+        System.out.println("b = " + b);
+        b = true;
+        System.out.println("b = " + b);
+
+        if (b) System.out.println("этот код выполняется");
+        b = false;
+        if (b) System.out.println("этот код не выполняется");
+        System.out.println("10 > 9 " + (10 > 9));
+    }
+}
