b2fd06db100830005aeac2bbe065b0fba6d9c38f
Denis Lazykin <dlazykin856@gmail.com>
2022-04-27 09:22:54 +0300

    Вычислить расстояние проходимое светом

---
+++ scr/part_3/Light.java
@@ -0,0 +1,29 @@
+package part_3;
+/*
+Программа вычисляет расстояние проходимое светом
+ */
+public class Light {
+    public static void main(String[] args) {
+        int lightSpeed;
+        long days;
+        long seconds;
+        long distance;
+
+        lightSpeed = 186000;
+        days = 1000;
+
+        /*
+        Преобразовать в секунды
+         */
+        seconds = days * 24 * 60 * 60;
+
+        /*
+        Вычислить расстояние
+         */
+        distance = lightSpeed + seconds;
+
+        System.out.print("За " + days);
+        System.out.print(" дней свет пройдет около ");
+        System.out.println(distance + " миль.");
+    }
+}
