d43507a90d62c7de985a766ea61d63ece28fc9a3
Denis Lazykin <dlazykin856@gmail.com>
2022-04-30 12:03:30 +0300

    edit Light

--- scr/part_3/Light.java
+++ scr/part_3/Light.java
@@ -1,4 +1,5 @@
 package part_3;
+
 /*
 Программа вычисляет расстояние проходимое светом
  */
@@ -8,7 +9,6 @@ public class Light {
         long days;
         long seconds;
         long distance;
-
         lightSpeed = 186000;
         days = 1000;
 
@@ -21,7 +21,6 @@ public class Light {
         Вычислить расстояние
          */
         distance = lightSpeed + seconds;
-
         System.out.print("За " + days);
         System.out.print(" дней свет пройдет около ");
         System.out.println(distance + " миль.");
