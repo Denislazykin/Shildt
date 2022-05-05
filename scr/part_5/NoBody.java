196aaff6b1239b64f9b4c5aff5135d403774daf4
Denis Lazykin <dlazykin856@gmail.com>
2022-05-05 18:07:52 +0300

    Демонстрация пустого цикла

---
+++ scr/part_5/NoBody.java
@@ -0,0 +1,14 @@
+package part_5;
+
+/*
+Демонстрация пустого цикла
+ */
+public class NoBody {
+    public static void main(String[] args) {
+        int i = 100;
+        int j = 200;
+        while (++i < --j);
+        System.out.println("Srednee znachenie = " + i);
+    }
+
+}
