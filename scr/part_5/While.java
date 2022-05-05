cc64175ca8910f482bac0cc8751facafba3b4029
Denis Lazykin <dlazykin856@gmail.com>
2022-05-05 18:04:49 +0300

    Демонстрация применения цикла while

---
+++ scr/part_5/While.java
@@ -0,0 +1,14 @@
+package part_5;
+
+/*
+Демонстрация применения цикла while
+ */
+public class While {
+    public static void main(String[] args) {
+        int n = 10;
+        while (n > 0) {
+            System.out.println("takt " + n);
+            n--;
+        }
+    }
+}
